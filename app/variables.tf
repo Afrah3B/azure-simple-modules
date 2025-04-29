aa20901c5d29f2ac4902d5315e88ac570447b062
afrah3b <afrahbawhab@gmail.com>
2025-04-29 15:42:50 +0300

    updated appgtw

--- app/variables.tf
+++ app/variables.tf
@@ -78,27 +78,27 @@ variable "tags" {
 }
 
 variable "lb_name" {}
-variable "frontend_ip_configuration_name" {}
-variable "frontend_public_ip_address_id" {}
 variable "lb_backend_pool_name" {}
 variable "lb_backend_address_name" {}
-variable "backend_ip_addresses" {
-  description = "List of backend VM private IP addresses to add to the backend pool."
-  type        = list(string)
-}
-variable "virtual_network_id" {}
 variable "lb_outbound_rule_name" {}
+variable "lb_rule_name" {}
+variable "frontend_port" {}
+variable "backend_port" {}
 variable "protocol" {
     type    = string
   default = "Tcp"
 }
-variable "lb_rule_name" {}
-variable "frontend_port" {}
-variable "backend_port" {}
 variable "lb_probe_path" {
     type    = string
   default = "/"
 }
+variable "frontend_public_ip_address_id" {}
+variable "virtual_network_id" {}
+variable "backend_ip_addresses" {
+  description = "List of backend VM private IP addresses to add to the backend pool."
+  type        = list(string)
+}
+variable "frontend_ip_configuration_name" {}
 
 
 variable "application_gateway_name" {}
@@ -121,4 +121,7 @@ variable "gateway_http_listener_name" {}
 variable "gateway_routing_rule_name" {}
 variable "gateway_probe_host" {}
 variable "gateway_probe_path" {}
-variable "appgw_public_ip_id" {}
+variable "appgw_public_ip_id" {
+    type    = string
+  default = "/"
+  }
